721d2b393c3e458793e244b9ade65dd9126cf498
Jdka1 <staryan.mehra@gmail.com>
2022-11-27 18:48:45 -0800

    Added tools

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/tools", methods=["POST","GET"])
-def login():
-    if request.method == "POST":
-        user = request.form["nm"]
-        flash("you are successfuly logged in")
-        return render_template("base.html")
-    else:
-        return render_template("tools.html")
+@app.route("/tools")
+def tools():
+    return render_template("tools.html")
 
+
+@app.route("/tools/<tool>")
+def tool(tool):
+    return render_template(f"{tool}.html")
 
 
 if __name__ == "__main__":
